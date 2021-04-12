251556d3cbb68c6f28f54e95a2a9d6459798c7d1
yhl88 <yhl898989@gmail.com>
2021-04-12 17:38:55 +0900

    Fix #23 - edit LogoPreview

--- src/components/modal/Preview.tsx
+++ src/components/modal/Preview.tsx
@@ -4,21 +4,19 @@ import { useSelector, useDispatch } from "react-redux";
 export default function Preview(props) {
   var origin: any = document.getElementById("my-canvas");
   var urlData = origin.toDataURL("image/png; base64");
-  var copy = document.getElementById("copy");
-  console.log(urlData);
 
   return (
     <React.Fragment>
       <div className="preview-container">
         <span id="WebPage" className="preview-item">
-          <img id="wpage1" src={urlData}></img>
+          <img id="web-page1" src={urlData}></img>
         </span>
         <span id="FaceBook" className="preview-item">
-          <img id="fbook1" src={urlData}></img>
+          <img id="face-book1" src={urlData}></img>
         </span>
         <span id="Bcard" className="preview-item">
-          <img id="bCard1" src={urlData}></img>
-          <img id="bCard2" src={urlData}></img>
+          <img id="b-card1" src={urlData}></img>
+          <img id="b-card2" src={urlData}></img>
         </span>
       </div>
     </React.Fragment>
